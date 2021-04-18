76fb07ba78751bad55b6691189ab77af2a93640f
TC <LiuTsungCheng@gmail.com>
2021-04-18 11:46:17 +0800

    quiet

---
+++ Examples/fakeCO.js
@@ -0,0 +1,43 @@
+function delay(seconds) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            resolve(seconds);
+        }, seconds * 1000);
+    });
+}
+
+function* gen() {
+    let a = yield delay(3);
+    console.log(a);
+    let b = yield delay(2);
+    console.log(b);
+    let c = yield delay(1);
+    console.log(c);
+}
+
+function co(it) {
+
+    return new Promise(function (resolve, reject) {
+
+        function next(data) {
+
+            let { value, done } = it.next(data);
+
+            if (done) {
+
+                resolve(value);
+
+            } else {
+
+                Promise.resolve(value).then((data) => {
+                    next(data);
+                });
+            }
+        }
+
+        next();
+    });
+}
+
+co(gen());
+
