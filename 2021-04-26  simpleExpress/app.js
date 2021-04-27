bbee65bf670caebac62c2e5019cf15802e015bc4
goldsir <LiuTsungCheng@gmail.com>
2021-04-27 08:28:15 +0800

    .

--- 2021-04-26  simpleExpress/app.js
+++ 2021-04-26  simpleExpress/app.js
@@ -7,21 +7,24 @@ let app = express();   //express 是一個函式 (一對圓括號調用)
 
 
 // 註冊一個路由: 匹配規則 
-app.get('/a', function (req, res) {
-    res.end('a');
+app.get('/a', function (req, res, next) {
+    res.write('1.a\n');
+    console.log('1');
+    next();
 });
 
-app.get('/b', function (req, res) {
-    res.end('b');
+app.get('/a', function (req, res, next) {
+    res.write('2.a\n');
+    console.log('2');
+    next();
+
 });
 
-app.get('/c', function (req, res) {
-    res.end('c');
+app.get('/a', function (req, res, next) {
+    res.end('3.a\n');
+    console.log('3');
 });
 
 app.listen(3000, function () {
     console.log('Server start at 3000 port');
 });
-
-
-// 中間件
