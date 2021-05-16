5b67afc71a708dc8619ae4eea40b3805c2666e3f
TC <LiuTsungCheng@gmail.com>
2021-05-16 17:11:11 +0800

    .

---
+++ 2021-05-16 TS/app.js
@@ -0,0 +1,7 @@
+var Gendor;
+(function (Gendor) {
+    Gendor[Gendor["man"] = 0] = "man";
+    Gendor[Gendor["woman"] = 1] = "woman";
+})(Gendor || (Gendor = {}));
+
+console.log(Gendor);
---
+++ 2021-05-16 TS/app.ts
@@ -0,0 +1,4 @@
+enum Gendor{
+    man
+    , woman
+}
