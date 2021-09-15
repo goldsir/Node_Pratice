4cad0b5ac407533df273c572994e8da0c2a8b3b8
goldsir <LiuTsungCheng@gmail.com>
2021-09-15 16:02:18 +0800

    .

--- 2021-09-11 message_board/modules/member.router.js
+++ 2021-09-11 message_board/modules/member.router.js
@@ -5,16 +5,16 @@ const router = require('express').Router();
 router.post('/register', inputValueCheck, async function (req, res, next) {
 
     let { account, password } = req.body;
-    let registerResult = await bll.register(account, password);
-    res.json(registerResult);
+    let result = await bll.register(account, password);
+    res.json(result);
 
 });
 
 router.post('/login', inputValueCheck, async function (req, res, next) {
 
     let { account, password } = req.body;
-    let registerResult = await bll.login(account, password);
-    res.json(registerResult);
+    let result = await bll.login(account, password);
+    res.json(result);
 
 });
 
