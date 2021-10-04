e38922b6785a1fb38da9e6abb298073eb7e15f5c
goldsir <LiuTsungCheng@gmail.com>
2021-10-04 08:48:03 +0800

    .

--- 2021-09-11 message_board/webpack.config.js
+++ 2021-09-11 message_board/webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 
-    mode: 'development' // production | development
+    mode: 'production' // production | development
 
     , entry: {
         common: path.join(__dirname, 'src/js/common.js')
