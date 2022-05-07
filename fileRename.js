5265fb4c71b9afa96a3923ee862372cbdcd17efc
TC <LiuTsungCheng@gmail.com>
2022-05-07 15:25:17 +0800

    .

--- fileRename.js
+++ fileRename.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-let _path = 'G:\\我的雲端硬碟\\教學視頻\\手寫vue3源碼_2'
+let base = 'G:\\我的雲端硬碟\\教學視頻';
+let dir = '手寫vue2.x源碼_3_小項目';
+let _path = path.join(base, dir);
+
 //22年最强手写Vue3源码【从零实现】-第1集-1_vue3的基本概念和设计理念
-let removePartReg = /^22年最强手写Vue3源码【从零实现】-第\d{1,2}集-/;
+let removePartReg = /【珠峰T0级别讲师】Vue2源码手写\+小项目-第\d{1,2}集-/;
 let extensionReg = /\.mp4$/;
 
 fs.readdir(_path, (err, filesName) => {
@@ -11,7 +14,6 @@ fs.readdir(_path, (err, filesName) => {
     if (err) return console.log(err);
 
     filesName.forEach(fileName => {
-
 
         let test1 = removePartReg.test(fileName);
         let test2 = extensionReg.test(fileName);
