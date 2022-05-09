3c9f56674dfe682e80524389eebc380052c2f9f8
goldsir <LiuTsungCheng@gmail.com>
2022-05-09 08:32:22 +0800

    .

---
+++ 2021-05-18-接帳整合_Express/EVO/evo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    casinoId: "spadewinbet00001"
+    , gameHistoryApiToken: "test123"
+    , baseURI: "https://spadewinbet.uat1.evo-test.com"
+    , historyAPI: "/api/gamehistory/v1/casino/games"
+
+}
